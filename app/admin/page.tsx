02e0bf6bf2e641d881b047590b0212de5601edb1
wilsondev94 <ohioleomole@gmail.com>
2024-12-10 23:56:32 +0000

    New change

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -11,16 +11,10 @@ async function Admin() {
   return (
     <div className="mx-auto flex max-w-7xl flex-col space-y-14 ">
       <header className="admin-header">
-        <Link href="/" className="cursor-pointer">
-          <Image
-            src="/assets/icons/logo-full.svg"
-            height={32}
-            width={32}
-            alt="logo"
-            className="h-8 w-fit"
-          />
-        </Link>
         <p className="text-16-semibold">Admin Dashboard</p>
+        <Link href="/" className="cursor-pointer">
+          <p className="text-16-semibold">Home</p>
+        </Link>
       </header>
       <main className="admin-main">
         <section className="w-full space-y-4">
